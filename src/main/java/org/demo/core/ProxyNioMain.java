e5eb0f89939b9987b0f1f3eb86f5638662d98c2a
Volodymyr Dvornyk <hagen-pereplut@ukr.net>
2014-10-29 21:26:56 +0100

    [NP-XX] added java entry point

---
+++ src/main/java/org/demo/core/ProxyNioMain.java
@@ -0,0 +1,10 @@
+package org.demo.core;
+
+/**
+ * Created by volodymyrd on 29.10.14.
+ */
+public class ProxyNioMain {
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
